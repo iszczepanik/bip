dcda45474bcea8615442f40fa941f935f6f7de9b
iszczepanik <iszczepanik@neubloc.net>
2013-03-03 18:34:03 +0100

    Mar-3 case sensitivity!

--- protected/views/externalControl/_history_link.php
+++ protected/views/externalControl/_history_link.php
@@ -2,5 +2,5 @@
 <a href="" class="rejestr-zmian" id="rejestr-zmian-ctrl-<? echo $data->CTRL_ID; ?>" >
 	<i class="icon-edit"></i>Rejestr zmian i metryczka</a>
 </div>
-<? $this->renderPartial('//ExternalControlHistory/admin', array('data'=>$data)); ?>
+<? $this->renderPartial('//externalControlHistory/admin', array('data'=>$data)); ?>
 
