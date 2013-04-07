de068b7630ca3e8c2f658375d1d243507c7b7fdd
Izabela Szczepanik <iszczepanik@neubloc.net>
2013-04-07 22:19:05 +0200

    Odpłatna działalność pożytku publicznego

--- protected/models/FinanceSource.php
+++ protected/models/FinanceSource.php
@@ -5,18 +5,21 @@ class FinanceSource
     const Proprietorship = 2;
 	const NaturalPersonDonation = 3;
 	const Project = 4;
+	const PaidPublic = 5;
 	
 	const PercentLabel = "1%";
     const ProprietorshipLabel = "Działalność gospodarcza";
 	const NaturalPersonDonationLabel = "Darowizny od osób fizycznych";
 	const ProjectLabel = "Na realizację projektów";
+	const PaidPublicLabel = "Odpłatna działalność pożytku publicznego";
 	
 	private static $enum = array
 	(
 		1 => "Wysokość środków pozyskanych z 1% podatku" , 
 		2 => "Wysokość środków uzyskanych w ramach działalności gospodarczej", 
 		3 => "Wysokość darowizn pozyskanych od osób fizycznych", 
-		4 => "Środki pozyskane na realizację projektów"
+		4 => "Środki pozyskane na realizację projektów",
+		5 => "Wysokość środków uzyskanych z odpłatnej działalności pożytku publicznego"
 	);
 
 	public static function GetOrdinal($name) 
@@ -36,6 +39,7 @@ class FinanceSource
 			FinanceSource::Proprietorship=>FinanceSource::ProprietorshipLabel,
 			FinanceSource::NaturalPersonDonation=>FinanceSource::NaturalPersonDonationLabel,
 			FinanceSource::Project=>FinanceSource::ProjectLabel,
+			FinanceSource::PaidPublic=>FinanceSource::PaidPublicLabel,
 		);
 	}
 }
--- protected/views/finance/links.php
+++ protected/views/finance/links.php
@@ -15,6 +15,9 @@
 	<a rel="tooltip" title="Dodaj" href="<? echo $this->createUrl('finance/create', array('type'=>$type,'source'=>FinanceSource::NaturalPersonDonation)); ?>" class="block right" >
 		<i class="icon-plus-sign"></i><? echo FinanceSource::GetDescription(FinanceSource::NaturalPersonDonation);  ?>
 	</a>
+	<a rel="tooltip" title="Dodaj" href="<? echo $this->createUrl('finance/create', array('type'=>$type,'source'=>FinanceSource::PaidPublic)); ?>" class="block right" >
+		<i class="icon-plus-sign"></i><? echo FinanceSource::GetDescription(FinanceSource::PaidPublic);  ?>
+	</a>
 <? endif; ?>
 	<a class="block right" href="<?php echo $this->createUrl('/finance/admin'); ?>">
 		<i class="icon-search"></i><i class="icon-pencil"></i><i class="icon-trash"></i>Panel administracyjny (wyszukiwanie,  edycja, usuwanie)
