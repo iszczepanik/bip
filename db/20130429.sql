eee8cfa66b32cafff046666ddadeaf28ac901120
Izabela Szczepanik <iszczepanik@neubloc.net>
2013-04-29 22:53:56 +0200

    AfterSave zamiast triggera - drop triggers

---
+++ db/20130429.sql
@@ -0,0 +1,5 @@
+DROP TRIGGER IF EXISTS `AddHistoryExternalControlRecord`;
+DROP TRIGGER IF EXISTS `AddHistoryFileRecord`;
+DROP TRIGGER IF EXISTS `AddHistoryFinanceRecord`;
+DROP TRIGGER IF EXISTS `AddHistoryInformationRecord`;
+DROP TRIGGER IF EXISTS `AddHistoryProjectRecord`;
