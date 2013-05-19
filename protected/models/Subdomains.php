b0c32674a249f510fc1b9f928ee4cc60c173e1a0
Izabela Szczepanik <iszczepanik@neubloc.net>
2013-05-19 16:27:59 +0200

    spddar

--- protected/models/Subdomains.php
+++ protected/models/Subdomains.php
@@ -3,13 +3,15 @@ class Subdomains
 {
     const example = 1;
 	const example2 = 2;
+	const spddar = 3;
     
     const exampleLabel = "example";
 	const example2Label = "example2";
+	const spddarLabel = "spddar";
 	
 	private static $enum = array
 	(
-		1 => "example" , 2 => "example2"
+		1 => "example" , 2 => "example2" , 3 => "spddar"
 	);
 
 	public static function GetOrdinal($name) 
@@ -27,6 +29,7 @@ class Subdomains
 		return array(
 			Subdomains::example=>Subdomains::exampleLabel, 
 			Subdomains::example2=>Subdomains::example2Label, 
+			Subdomains::spddar=>Subdomains::spddarLabel, 
 		);
 	}
 }
