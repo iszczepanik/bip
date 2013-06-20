3a5a6a32f3622c1e1f9aea9016346d7bd675732a
Izabela Szczepanik <iszczepanik@neubloc.net>
2013-06-20 21:05:36 +0200

    version 1.0.6

--- protected/config/main.php
+++ protected/config/main.php
@@ -139,7 +139,7 @@ return array(
 		'debug'=>false,
 		'organization'=>'Bona Fides',
 		'homepage'=>'http://bonafides.pl',
-		'version'=>'1.0.5',
+		'version'=>'1.0.6',
 		'noindex'=>false,
 		
 		'subdomains'=>array
