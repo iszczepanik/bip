36781c24c5738a1837323e5096d71e5d270b5852
Izabela Szczepanik <iszczepanik@neubloc.net>
2013-06-23 10:54:18 +0200

    Naprawiony link do dokumentów

--- protected/views/externalControl/_form.php
+++ protected/views/externalControl/_form.php
@@ -68,7 +68,7 @@
 		<?php echo $form->labelEx($model,'CTRL_FILE_ID',array('class'=>'control-label')); ?>
 		<div class="controls">
 		<div class="alert alert-info">Wrzuć plik na serwer w 
-		<strong><a href="<?echo $this->createUrl('/Files/admin');?>" >panelu zarządzania dokumentami</a></strong> z kategorią "Wyniki pokontrolne". Wówczas plik znajdzie się na poniższej liście i będzie można go powiązać z kontrolą zewnętrzną.
+		<strong><a href="<?echo $this->createUrl('/FileAdmin/admin');?>" >panelu zarządzania dokumentami</a></strong> z kategorią "Wyniki pokontrolne". Wówczas plik znajdzie się na poniższej liście i będzie można go powiązać z kontrolą zewnętrzną.
 		</div>
 		<?php echo $form->dropDownList($model, 'CTRL_FILE_ID', CHtml::listData(
 			File::model()->findAllByAttributes(array('FIL_CAT'=>FileCategory::ControlResult,'FIL_APP_ID'=>Yii::app()->request->subdomainAppId)), 'FIL_ID', 'FIL_NAME')
