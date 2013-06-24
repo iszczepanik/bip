319119bb855bfbdd3586baf9747d7b91782407c5
Izabela Szczepanik <iszczepanik@neubloc.net>
2013-06-24 22:17:02 +0200

    Poprawka AddNGO

--- db/20130429.sql
+++
@@ -1,5 +0,0 @@
-DROP TRIGGER IF EXISTS `AddHistoryExternalControlRecord`;
-DROP TRIGGER IF EXISTS `AddHistoryFileRecord`;
-DROP TRIGGER IF EXISTS `AddHistoryFinanceRecord`;
-DROP TRIGGER IF EXISTS `AddHistoryInformationRecord`;
-DROP TRIGGER IF EXISTS `AddHistoryProjectRecord`;
