281ff6308dc5a9cfe30cbea6fa1cee2410af8b52
Izabela Szczepanik <iszczepanik@neubloc.net>
2013-07-04 21:53:20 +0200

    version 1.0.10 Poprawka w bazie

--- protected/config/main.php
+++ protected/config/main.php
@@ -139,7 +139,7 @@ return array(
 		'debug'=>false,
 		'organization'=>'Bona Fides',
 		'homepage'=>'http://bonafides.pl',
-		'version'=>'1.0.9',
+		'version'=>'1.0.10',
 		'noindex'=>false,
 		
 		'subdomains'=>array
