d1d7fefe4cbba7dcd6611c6e3c9835a76e46bf61
Izabela Szczepanik <iszczepanik@neubloc.net>
2013-08-11 11:19:06 +0200

    Waluty, version 1.0.13

--- protected/config/main.php
+++ protected/config/main.php
@@ -139,7 +139,7 @@ return array(
 		'debug'=>false,
 		'organization'=>'Bona Fides',
 		'homepage'=>'http://bonafides.pl',
-		'version'=>'1.0.12',
+		'version'=>'1.0.13',
 		'noindex'=>false,
 		
 		'subdomains'=>array
