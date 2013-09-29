0d4e273c0a86e022faeee8ebea272f5f21d4d64c
Izabela Szczepanik <iszczepanik@neubloc.net>
2013-09-29 17:46:33 +0200

    cleared config

--- protected/config/main.php
+++ protected/config/main.php
@@ -9,6 +9,8 @@ return array(
 	'name'=>'Biuletyn Informacji Publicznej',
 	'sourceLanguage'=>'pl',
 	'language'=>'pl',
+	//'debug'=>true,
+	// preloading 'log' component
 	'preload'=>array('log','bootstrap'),
 
 	// autoloading model and component classes
@@ -22,10 +24,12 @@ return array(
 		// uncomment the following to enable the Gii tool
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'///',
+			'password'=>'tr@mwaj',
 			'generatorPaths'=>array(
-			'bootstrap.gii', // since 0.9.1
+			            'bootstrap.gii', // since 0.9.1
 			),
+		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			//'ipFilters'=>array('127.0.0.1','::1'),
 		),
 	),
 
@@ -35,20 +39,29 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 			'class' => 'WebUser',
+			//'identityCookie' => array('domain' => 'localhost.com')
 		),
 		'session' => array(
 			'autoStart'=>true,
+			//'cookieParams' => array('domain' => 'localhost.com'),
 			'sessionName' => 'DomainSession',
 		),
 		'bootstrap'=>array(
 		        'class'=>'ext.bootstrap.components.Bootstrap', // assuming you extracted bootstrap under extensions
 		),
-
+		
         'request' => array(
 			'class'=>'CHttpRequestSubDomain',
+            //'baseUrl' => 'http://localhost/bip',
+			//'baseUrl' => 'http://bonafides.pl/bip/',
         ),
+		
+		/*'file'=>array(
+			'class'=>'application.extensions.file.CFile',
+		),*/
 
 		// uncomment the following to enable URLs in path-format
+		
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -57,20 +70,27 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=bipnij',
-			'emulatePrepare' => true,
-			'username' => 'your_username',
-			'password' => 'your_password',
-			'charset' => 'utf8',
+					'connectionString' => 'mysql:host=localhost;dbname=bonafide_bip',
+					'emulatePrepare' => true,
+					'username' => 'bonafide_bip',
+					'password' => 'tr@mw@j',
+					'charset' => 'utf8',
 		),
-
 		'authManager'=>array(
 			'class'=>'CDbAuthManager',
 			'connectionID'=>'db',
 		),
-
+		/* server wehost */
+		// 'db'=>array(
+			// 'connectionString' => 'mysql:host=sql5.projekty.pl;dbname=isabel_sysrep-tw', 
+			// 'emulatePrepare' => true,
+			// 'username' => 'isabel_sysreptw',
+			// 'password' => 'Ecl|p$e',
+			// 'charset' => 'utf8',
+		// ),
+		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
             'errorAction'=>'site/error',
@@ -82,6 +102,12 @@ return array(
 					'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
 				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
 			),
 		),
 	),
@@ -92,14 +118,13 @@ return array(
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',
 		'debug'=>false,
-		'organization'=>'Bona Fides',
-		'homepage'=>'http://bonafides.pl',
+		'organization'=>'Stowarzyszenie Bona Fides',
 		'version'=>'1.0.18',
-		'noindex'=>false,
+		'noindex'=>true,
+		
 		'subdomains'=>array
 		(
-			1 => "example", 
+			1 => "bonafides", 
 		)
-
 	),
 );
